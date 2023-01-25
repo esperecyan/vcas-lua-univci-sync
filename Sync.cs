87cc3ec748b1e9c3ff449c0803a2331f83853aa0
100の人 <100@pokemori.jp>
2023-01-25 14:52:27 +0900

    初期実装

---
+++ Sync.cs
@@ -0,0 +1,90 @@
+using System;
+using System.IO;
+using System.Threading;
+using UnityEditor;
+using VCI;
+
+namespace Esperecyan.VCasLuaUniVCISync
+{
+    internal static class Sync
+    {
+        private static readonly string SourceFolderPath
+            = @"%USERPROFILE%\AppData\LocalLow\infiniteloop Co,Ltd\VirtualCast\EmbeddedScriptWorkspace";
+
+        private static SynchronizationContext MainThreadContext;
+        private static string SourceFolderFullPath;
+        private static FileSystemWatcher Watcher;
+
+        [InitializeOnLoadMethod]
+        private static void Run()
+        {
+            if (Sync.Watcher != null)
+            {
+                return;
+            }
+
+            Sync.MainThreadContext = SynchronizationContext.Current;
+
+            Sync.SourceFolderFullPath = Environment.ExpandEnvironmentVariables(Sync.SourceFolderPath);
+
+            Sync.Watcher = new FileSystemWatcher()
+            {
+                Path = Sync.SourceFolderFullPath,
+                Filter = "*.lua",
+                IncludeSubdirectories = true,
+                EnableRaisingEvents = true,
+            };
+            Sync.Watcher.Changed += Sync.Watcher_Changed;
+        }
+
+        private static void Watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            var parentFolderPath = Path.GetDirectoryName(e.FullPath);
+            if (Path.GetDirectoryName(parentFolderPath) != Sync.SourceFolderFullPath)
+            {
+                return;
+            }
+
+            var vciName = Path.GetFileName(parentFolderPath);
+            Sync.MainThreadContext.Post(_ =>
+            {
+                foreach (var guid in AssetDatabase.FindAssets(vciName + " t:Prefab", new[] { "Assets" }))
+                {
+                    var path = AssetDatabase.GUIDToAssetPath(guid);
+
+                    if (Path.GetFileNameWithoutExtension(path) != vciName)
+                    {
+                        continue;
+                    }
+
+                    var vciObject = AssetDatabase.LoadAssetAtPath<VCIObject>(path);
+                    if (vciObject == null)
+                    {
+                        continue;
+                    }
+
+                    var scriptName = Path.GetFileNameWithoutExtension(e.FullPath);
+                    var script = vciObject.Scripts.Find(s => s.name == scriptName);
+                    if (script == null || !string.IsNullOrEmpty(script.filePath))
+                    {
+                        break;
+                    }
+
+                    var code = File.ReadAllText(e.FullPath);
+                    if (script.textAsset != null)
+                    {
+                        var luaAssetPath = AssetDatabase.GetAssetPath(script.textAsset);
+                        File.WriteAllText(Path.GetFullPath(luaAssetPath), code);
+                        AssetDatabase.ImportAsset(luaAssetPath);
+                    }
+                    else
+                    {
+                        script.source = code;
+                    }
+
+                    break;
+                }
+            }, state: null);
+        }
+    }
+}
